d352e6c0feaaff5f0d5da4f9477f76cb33ca2556
Camilo Cardenas <jcardenasc93@gmail.com>
2019-10-02 14:39:04 -0500

    Agrega boton al finalizar el pedido para volver a ordenar. Adicionalmente se agrega el mecanismo de valoracion para la app

--- lib/pages/order_resume.dart
+++ lib/pages/order_resume.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import "package:intl/intl.dart";
+import 'package:smooth_star_rating/smooth_star_rating.dart';
 
 import '../models/platos_model.dart';
+import '../pages/home.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class OrderResume extends StatelessWidget {
+class OrderResume extends StatefulWidget {
   /// The list of dishes in final order.
   final List<Plato> orden;
 
@@ -20,15 +22,35 @@ class OrderResume extends StatelessWidget {
   /// The Order ID.
   final int orderID;
 
-  /// The price wiht currency format.
-  final formatPrice = new NumberFormat.simpleCurrency(decimalDigits: 0);
-
   OrderResume(
       {this.idRestaurante,
       this.idMesa,
       this.orden,
       this.valorTotal,
       this.orderID});
+
+  @override
+  State<StatefulWidget> createState() {
+    return OrderResumeState(orden: orden);
+  }
+}
+
+class OrderResumeState extends State<OrderResume> {
+  List<Plato> orden;
+  OrderResumeState({this.orden});
+
+  /// The price wiht currency format.
+  final formatPrice = new NumberFormat.simpleCurrency(decimalDigits: 0);
+
+  /// The rate value given by the user
+  double rate = 5.0;
+
+  void _returnHome(context) {
+    Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage()),
+        (Route<dynamic> route) => false);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,118 +67,140 @@ class OrderResume extends StatelessWidget {
     return Scaffold(
         // Disable back button on appbar
         body: WillPopScope(
-      onWillPop: () async {
-        return Future.value(false);
-      },
-      child: ListView(
-        children: <Widget>[
-          // Create a scrollable ListView with the order list.
-          Padding(
-            padding: EdgeInsets.all(ScreenUtil.instance.setWidth(15.0)),
-            // Total order remarkable.
-            child: Container(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Text(
-                    'TOTAL: ${formatPrice.format(valorTotal)}',
-                    style: TextStyle(
+          onWillPop: () async {
+            return Future.value(false);
+          },
+          child: ListView(
+            children: <Widget>[
+              // Create a scrollable ListView with the order list.
+              Padding(
+                padding: EdgeInsets.all(ScreenUtil.instance.setWidth(15.0)),
+                // Total order remarkable.
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Text(
+                        'TOTAL: ${formatPrice.format(widget.valorTotal)}',
+                        style: TextStyle(
+                          color: Color(0xFF66666F),
+                          fontSize: ScreenUtil.instance.setSp(38.0),
+                        ),
+                      ),
+                      Text(
+                        'Tu orden ha sido confirmada',
+                        style: TextStyle(
+                            fontSize: ScreenUtil.instance.setSp(18.0),
+                            color: Color(0xFF66666F)),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              // Custom divider.
+              Padding(
+                padding:
+                    EdgeInsets.only(bottom: ScreenUtil.instance.setWidth(3.0)),
+                child: Container(
+                    width: _mediaQueryData.size.width,
+                    height: ScreenUtil.instance.setWidth(25.0),
+                    color: Color(0xFF00E676),
+                    child: Center(
+                      child: Text(
+                        'Orden No.${widget.orderID}',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: ScreenUtil.instance.setSp(20.0),
+                        ),
+                      ),
+                    )),
+              ),
+              ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  physics: ClampingScrollPhysics(),
+                  itemCount: orden.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      // Display dish name.
+                      title: new Text(
+                        orden[index].nombrePlato,
+                        style: new TextStyle(
+                          fontSize: ScreenUtil.instance.setSp(16.0),
+                        ),
+                      ),
+                      trailing:
+                          // Display dish price.
+                          new Text(
+                        'x${orden[index].cantidad}  ${formatPrice.format(orden[index].precioTotalPlato)}',
+                        style: new TextStyle(
+                          color: Color(0xFF66666F),
+                          fontSize: ScreenUtil.instance.setSp(14.0),
+                        ),
+                        textAlign: TextAlign.end,
+                      ),
+                    );
+                  }),
+              Padding(
+                padding:
+                    EdgeInsets.only(top: ScreenUtil.instance.setHeight(25.0)),
+                // Acknowledgment text
+                child: Text(
+                  'Gracias por usar nuestro servicio',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
                       color: Color(0xFF66666F),
-                      fontSize: ScreenUtil.instance.setSp(38.0),
-                    ),
-                  ),
-                  Text(
-                    'Tu orden ha sido confirmada',
-                    style: TextStyle(
-                        fontSize: ScreenUtil.instance.setSp(18.0),
-                        color: Color(0xFF66666F)),
-                  ),
-                ],
+                      fontSize: ScreenUtil.instance.setSp(18.0),
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              Container(
+                alignment: Alignment.bottomCenter,
+                child: // Rate mechanism.
+                    SmoothStarRating(
+                  allowHalfRating: false,
+                  onRatingChanged: (r) {
+                    rate = r;
+                    setState(() {});
+                  },
+                  starCount: 5,
+                  rating: rate,
+                  size: ScreenUtil.instance.setSp(35.0),
+                  color: Color(0xFF00E676),
+                  borderColor: Color(0xFF00E676),
+                  spacing: 0.5,
+                ),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: new Container(
+          color: Colors.white,
+          child: Padding(
+            padding: EdgeInsets.all(ScreenUtil.instance.setWidth(8.0)),
+            child: RaisedButton(
+              onPressed: () {
+                _returnHome(context);
+              },
+              child: Text(
+                'Nueva Orden',
+                style: TextStyle(
+                  color: Color(0xFF00E676),
+                  fontSize: ScreenUtil.instance.setSp(20.0),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              color: Colors.white,
+              highlightColor: Colors.white54,
+              elevation: 1.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
+                side: BorderSide(color: Color(0xFF00E676), width: 2.0),
               ),
             ),
           ),
-          // Custom divider.
-          Padding(
-            padding: EdgeInsets.only(bottom: ScreenUtil.instance.setWidth(3.0)),
-            child: Container(
-                width: _mediaQueryData.size.width,
-                height: ScreenUtil.instance.setWidth(25.0),
-                color: Color(0xFF00E676),
-                child: Center(
-                  child: Text(
-                    'Orden No.$orderID',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: ScreenUtil.instance.setSp(20.0),
-                    ),
-                  ),
-                )),
-          ),
-          ListView.builder(
-              scrollDirection: Axis.vertical,
-              shrinkWrap: true,
-              physics: ClampingScrollPhysics(),
-              itemCount: orden.length,
-              itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                  // Display dish name.
-                  title: new Text(
-                    orden[index].nombrePlato,
-                    style: new TextStyle(
-                      fontSize: ScreenUtil.instance.setSp(16.0),
-                    ),
-                  ),
-                  trailing:
-                      // Display dish price.
-                      new Text(
-                    'x${orden[index].cantidad}  ${formatPrice.format(orden[index].precioTotalPlato)}',
-                    style: new TextStyle(
-                      color: Color(0xFF66666F),
-                      fontSize: ScreenUtil.instance.setSp(14.0),
-                    ),
-                    textAlign: TextAlign.end,
-                  ),
-                );
-              }),
-          Padding(
-            padding: EdgeInsets.only(top: ScreenUtil.instance.setHeight(25.0)),
-            // Acknowledgment text
-            child: Text(
-              'Gracias por usar nuestro servicio',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  color: Color(0xFF66666F),
-                  fontSize: ScreenUtil.instance.setSp(18.0),
-                  fontWeight: FontWeight.bold),
-            ),
-          ),
-          // Rate mechanism.
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Icon(Icons.star_border,
-                  size: ScreenUtil.instance.setSp(30.0),
-                  color: Color(0xFF00E676)),
-              Icon(Icons.star_border,
-                  size: ScreenUtil.instance.setSp(30.0),
-                  color: Color(0xFF00E676)),
-              Icon(Icons.star_border,
-                  size: ScreenUtil.instance.setSp(30.0),
-                  color: Color(0xFF00E676)),
-              Icon(Icons.star_border,
-                  size: ScreenUtil.instance.setSp(30.0),
-                  color: Color(0xFF00E676)),
-              Icon(Icons.star_border,
-                  size: ScreenUtil.instance.setSp(30.0),
-                  color: Color(0xFF00E676))
-            ],
-          )
-        ],
-      ),
-    ));
+        ));
   }
 }
