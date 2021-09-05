ef76412dc4258266b13ca723415ff6d9a90d8dd6
jcardenasc93 <jcardenasc93@gmail.com>
2021-09-04 21:47:11 -0500

    Add external url image for Restaurant logo

--- lib/models/restaurante_model.dart
+++ lib/models/restaurante_model.dart
@@ -4,18 +4,24 @@ class Restaurante {
   final int id;
   final String nombreRestaurante;
   final String urlImagRestaurante;
+  final String externalImage;
   final List<Menu> menus;
 
-  Restaurante({this.id, this.nombreRestaurante, this.urlImagRestaurante, this.menus});
+  Restaurante(
+      {this.id,
+      this.nombreRestaurante,
+      this.urlImagRestaurante,
+      this.externalImage,
+      this.menus});
 
   factory Restaurante.fromJson(Map<String, dynamic> parsedJson) {
     var list = parsedJson['menu_restaurante'] as List;
     List<Menu> menusList = list.map((i) => Menu.fromJson(i)).toList();
     return Restaurante(
-      id: parsedJson['id'],
-      nombreRestaurante: parsedJson['nombre_restaurante'],
-      urlImagRestaurante: parsedJson['url_imagen_restaurante'],
-      menus: menusList
-    );
+        id: parsedJson['id'],
+        nombreRestaurante: parsedJson['nombre_restaurante'],
+        urlImagRestaurante: parsedJson['url_imagen_restaurante'],
+        externalImage: parsedJson['url_imagen_restaurante'],
+        menus: menusList);
   }
 }
